54023091e4c545815f959acd0dd9ad84a146be24
helia <66224930+HeliaRasooli@users.noreply.github.com>
2020-07-19 23:53:20 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import './App.css';
+import Game from './components/Game';
 
+function App() {
+  
+  return (
+   <Game />
+  );
+}
+
+export default App;
