05a51584aff1d0970499b512e845bc2a9c03b979
helia <66224930+HeliaRasooli@users.noreply.github.com>
2020-07-20 00:01:42 +0430

    Update Square.js

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,1 +1,10 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
 
+export default function Square(props) {
+    return (
+        <Button className="square" variant="outlined" color="primary" onClick={props.onClick}>
+            {props.value}
+        </Button>
+    )
+}
